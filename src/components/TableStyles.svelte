2260639cbfb41c71248284a1d7f5d3e535c49bf0
Ricardo <richvclaros@gmail.com>
2022-06-18 20:01:59 +0200

    TableStyles: Globalize CSS for tables

---
+++ src/components/TableStyles.svelte
@@ -0,0 +1,45 @@
+<style>
+    :global(input) {
+		display: block;
+		margin:  1em 1em;
+	}
+
+    :global(label) {
+        color: back;
+        background: rgba(231, 231, 231, 0.994);
+        padding: 0rem 0rem;
+        border-radius: 10px;
+        width: 20em;
+        margin: 0 1em;
+    }
+
+    :global(select) {
+        float: left;
+        margin: 0 1em 1em 0;
+        width: 20em;
+
+    }
+    :global(table) {
+        background: rgba(118, 116, 130, 0.994);
+        color: rgb(255, 255, 255);
+        border-radius: 0 1em 1em 0;
+        margin: 0em 1em;
+    }    
+    :global(tr) {
+        border-color: lighten(#ffffff, 10%);
+        text-align: left;
+    }
+    :global(table th){
+        color: rgb(255, 255, 255);
+    }
+    
+    :global(th,
+    td) {
+        padding: 6.5px 0.5em;
+    }
+    :global(.buttons){
+        clear: both;
+        margin: 1em 1em;
+    }
+
+</style>  
