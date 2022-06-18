515d1f47652fc378dd29d9289ee66a8a13cf1c22
Ricardo <richvclaros@gmail.com>
2022-06-19 00:46:10 +0200

    Adde: Routes and Links with "svelte-routing" lib

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,54 @@
 <!-- https://eugenkiss.github.io/7guis/tasks#crud -->
 <script>
+import {Router, Route, Link} from "svelte-routing";
 import ComptesList from "./components/ComptesList.svelte";
 import CarpetesList from "./components/CarpetesList.svelte";
 import TableStyles from "./components/TableStyles.svelte";
-export let menu = 1;
 </script>
-
-
-
-
-
 
 <style>
     :global(*) {
             box-sizing: border-box;
             padding: 0;
-            margin: 0;
+            margin: 0 1em;
     }
     :global(body) {
         color: hsl(0, 0%, 51%);
         background: rgba(40, 39, 49, 0.994)    }
-    
-    ul#menu li{
-		display : inline;
-        margin : 0 1em;
-	}
+    header {
+        margin: 0em -1em;
+    }
+    h3{
+        margin: 0 0;
+        margin-top: 3em;
+       
+    }
 </style>
+<TableStyles/>
 
+<Router>
+    <header>
+        <h1>LockMaster</h1>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="comptes">Comptes</Link>
+            <Link to="carpetes">Carpetes</Link>
+        </nav>
+    </header>
+    
+    <main>
+        <Route path="/">
+            <h3>Home</h3>
+            <p>This is the home page.</p>
+        </Route>
+    
+        <Route path="comptes">
+            <ComptesList />
+        </Route>
+    
+        <Route path="carpetes">
+            <CarpetesList />
+        </Route>
+    </main>
+</Router>
 
-
-<ul id="menu">
-	<li><a href="/comptes" on:click|preventDefault={() => (menu = 1)}>ComptesList</a></li> |
-	<li><a href="/carpetes" on:click|preventDefault={() => (menu = 2)}>CarpetesList</a></li>
-</ul>
-
-
-<TableStyles />
-
-{#if menu === 1}
-<ComptesList />
-{:else if menu === 2}
-<CarpetesList />
-{:else}
-<h1>
-	Page Not Found
-</h1>
-{/if}
